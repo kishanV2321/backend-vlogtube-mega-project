d3c7505083a4dc3984f826042d441ff4bfb4df76
Kishan Verma <kv230801@gmail.com>
2024-04-16 15:49:10 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -12,9 +12,6 @@ app.use(cors( {
 }
 ))
 
-app.use('/', (req,res) => {
-    res.send("Server is running")
-})
 
 app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({extended: true, limit: "50mb"}))
