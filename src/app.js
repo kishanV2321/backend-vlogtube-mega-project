5e62f6d0db5790b0d839599287a3aaa8f08ba267
Kishan Verma <kv230801@gmail.com>
2024-04-16 15:02:45 +0530

    update app.js file

--- src/app.js
+++ src/app.js
@@ -7,10 +7,14 @@ const app = express();
 
 //app.use -> used for confiquration and middlewares 
 app.use(cors( {
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }
 ))
+
+app.use('/', (req,res) => {
+    res.send("Server is running")
+})
 
 app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({extended: true, limit: "50mb"}))
